8bfaae976d0befaebd48ec7c1c72bbe67f1e61cd
sham <khaj3172@gmail.com>
2023-06-19 09:34:44 +0500

    changed the links to external links

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [react()],	
 	build: {
 		rollupOptions: {
-		  external: ['react-icons/bs'],
+		  external: ['react-icons/bs',"firebase/app" , "firebase/firestore/lite"],
 		},
 	  },
 })
